6461ae06180e395ff7a0ca798a3a18a94bea5910
Blaire Baker <bakerblaire@gmail.com>
2021-08-04 16:34:50 -0600

    update comments

--- index..js
+++ index..js
@@ -1,18 +1,31 @@
 function funWithAnagrams(array) {
-    let sortedArray = array.map(string =>       
-       string.split('').sort().join(''))
+
+    // sort all strings in alphabetical order so you can compare if they are anagrams
+    let sortedArray = array.map(string => string.split('').sort().join(''))
+    // an array that contains the first instance of each anagram
+    // the first string in the array will always be included 
     let firstAnagram = [sortedArray[0]]
+    // an array that containes the indices of each anagram
+    // index of 0 will always be included
     let indices = [0]
+    // an empty array of the final results I want to return at the end
     let results = []
+
+    // iterate through sortedArray
     for (let i = 1; i < sortedArray.length; i++) {
+        // if string is not included in firstAnagram array, add to array and its index to indices array
        if (firstAnagram.includes(sortedArray[i])) {
        } else {
           firstAnagram.push(sortedArray[i])
           indices.push(i)
        }
     }
+
+    //iterate through indices and push string from original array at that index onto results array
     indices.forEach(index => {
        results.push(array[index])
     })
+
+    //sort results array in alphabetical order
     return results.sort()
  }
